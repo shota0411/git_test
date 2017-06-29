219b03cb5142cdbcec834030b506998ceb167d8a
松田翔大 <thank2626@gmail.com>
2017-06-30 03:00:05 +0900

    gameover

---
+++ Assets/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver: MonoBehaviour 
+{
+
+	[SerializeField] private Text gameoverText;
+	[SerializeField] private GameObject ball;
+	void Start () 
+	{		
+		
+	}
+
+	void Update () 
+	{
+		if(ball.transform.position.y < 0.4){
+			gameoverText.text = "GameOver!!";
+		}
+	}
+
+
+}
