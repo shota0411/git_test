4af6c0337bc09a4bd9f7d51bb4bcd8c3d42d37e9
松田翔大 <thank2626@gmail.com>
2017-06-30 02:16:33 +0900

    ball script

---
+++ Assets/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speedx = Input.GetAxisRaw ("Horizontal");
+		float speedz = Input.GetAxisRaw ("Vertical");
+
+		rigidbody.AddForce (Vector3.forward*speedz, ForceMode.Force);
+		rigidbody.AddForce (Vector3.right*speedx, ForceMode.Force);
+	}
+}
